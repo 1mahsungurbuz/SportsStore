e6e079d6fc693c0ae11050572985ad4da2a6bcc4
Mahsun Gürbüz <83517140+1mahsungurbuz@users.noreply.github.com>
2024-02-26 13:04:37 +0300

    Add project files.

---
+++ Migrations/20240107094854_MyFinalMig.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class MyFinalMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models {
+
+    public static class SeedData {
+
+        public static void EnsurePopulated(IApplicationBuilder app) {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any()) {
+                context.Database.Migrate();
+            }
+
+            if (!context.Products.Any()) {
+                context.Products.AddRange(
+                    new Product {
+                        Name = "Kayak", Description = "A boat for one person",
+                        Category = "Watersports", Price = 275,
+                        ImageName = "Kayak.webp"
+					},
+                    new Product {
+                        Name = "Lifejacket",
+                        Description = "Protective and fashionable",
+                        Category = "Watersports", Price = 48.95m,
+                        ImageName = "Lifejacket.webp"
+					},
+                    new Product {
+                        Name = "Soccer Ball",
+                        Description = "FIFA-approved size and weight",
+                        Category = "Soccer", Price = 19.50m,
+                        ImageName = "SoccerBall.webp"
+					},
+                    new Product {
+                        Name = "Corner Flags",
+                        Description = "Give your playing field a professional touch",
+                        Category = "Soccer", Price = 34.95m,
+                        ImageName = "CornerFlags.jpg"
+					},
+                    new Product {
+                        Name = "Stadium",
+                        Description = "Flat-packed 35,000-seat stadium",
+                        Category = "Soccer", Price = 79500,
+                        ImageName = "Stadium.jpg"
+					},
+                    new Product {
+                        Name = "Thinking Cap",
+                        Description = "Improve brain efficiency by 75%",
+                        Category = "Chess", Price = 16,
+                        ImageName = "ThinkingCap.jpg"
+					},
+                    new Product {
+                        Name = "Unsteady Chair",
+                        Description = "Secretly give your opponent a disadvantage",
+                        Category = "Chess", Price = 29.95m,
+                        ImageName = "UnsteadyChair.webp"
+					},
+                    new Product {
+                        Name = "Human Chess Board",
+                        Description = "A fun game for the family",
+                        Category = "Chess", Price = 75,
+                        ImageName = "HumanChessBoard.webp"
+					},
+                    new Product {
+                        Name = "Bling-Bling King",
+                        Description = "Gold-plated, diamond-studded King",
+                        Category = "Chess", Price = 1200,
+                        ImageName = "BlingBlingKing.webp"
+
+					}
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
